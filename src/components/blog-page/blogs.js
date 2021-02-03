61d23d8f637a8009e45626e24550b205027ede3b
stuart <stupop010@gmail.com>
2021-02-03 14:07:12 +0000

    dont display dummy post

--- src/components/blog-page/blogs.js
+++ src/components/blog-page/blogs.js
@@ -6,6 +6,9 @@ const Blogs = ({ blogs }) => {
   return (
     <div className="blogs">
       {blogs.map(({ node }) => {
+        // If node id equals 21247c81-2514-5186-a3f8-99c076a93e47 its the dummy blog post
+        if (node.id === "21247c81-2514-5186-a3f8-99c076a93e47") return null
+
         return (
           <div className="blog-card" key={node.id}>
             <Img fluid={node.image.fluid} />
